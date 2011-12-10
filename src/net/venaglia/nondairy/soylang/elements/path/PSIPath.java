24d9c6fc614b08056efe65d86fc01645bdfc9227
Ed Venaglia <evenagli@gmail.com>
2011-12-10 09:45:40 -0800

    Updated tests to be a bit more thourough in an unsuccessful attempt to reproduce the bugs in the current release; Initial changes to allow the plugin to work in Idea 11

---
+++ src/net/venaglia/nondairy/soylang/elements/path/PSIPath.java
@@ -0,0 +1,12 @@
+package net.venaglia.nondairy.soylang.elements.path;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ed
+ * Date: Aug 26, 2010
+ * Time: 7:28:11 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PSIPath {
+
+}
