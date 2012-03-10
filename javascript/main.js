ace57897bb73ed8ed5d5391c19646c1424e39d4a
Ed Venaglia <evenagli@gmail.com>
2012-03-09 23:26:22 -0800

    Updating visual style of main page.

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,29 @@
 $(function() {
-	$('a.gallery').lightBox(); // Select all links with lightbox class
+    $('.feature a.gallery').click(function(e) {
+        e.preventDefault();
+        var feature = $(this).parentsUntil(".feature").parent();
+        var caption = feature.find("h3").text();
+        var images = [];
+        var items = feature.find("a.gallery");
+        var index = 0;
+        for (var i = 0, l = items.length; i < l; ++i) {
+            var  item = $(items[i]);
+            var label = item.attr('title') || item.attr('alt');
+            images.push({
+                'href': item.attr('href'),
+                'title': label || caption
+            });
+            if (this === item[0]) {
+                index = i;
+            }
+        }
+
+        $.fancybox(images, {
+            'cyclic': true,
+            'padding': 0,
+            'transitionIn': 'elastic',
+            'transitionOut': 'elastic',
+            'index': index
+        });
+    });
 });
